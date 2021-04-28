0224b232f781058055fb84a0d6e3b86612418058
Kazak Kirill <kazzak.kirill1995@gmail.com>
2021-04-28 11:36:57 +0300

    Have a problem. Dont start

--- app/src/main/java/com/example/lecture20/MainActivity.kt
+++ app/src/main/java/com/example/lecture20/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.lecture20
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import java.util.concurrent.Callable
+import java.util.concurrent.FutureTask
 
 class MainActivity : AppCompatActivity() {
 
-    val month = 3;
-    var season : String? = null;
-    val editText : EditText
+    val month = 3
+    var season : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +27,31 @@ class MainActivity : AppCompatActivity() {
             else -> season = "Не знаю"
         }
         Log.d("TAG", season.toString())
+
+        val textView : TextView = findViewById(R.id.text_view)
+        val editText : EditText = findViewById(R.id.edit_text)
+        val button : Button = findViewById(R.id.button)
+
+        button.setOnClickListener(View.OnClickListener {
+            fun <Unit> onClick() {
+                var callable : Callable<kotlin.Unit> = getDataFromCallable(editText)
+                var futureTask : FutureTask<kotlin.Unit> = FutureTask(callable)
+                Thread(futureTask).start()
+                textView.setText(futureTask.get().toString())
+            }
+        })
+    }
+
+    fun getDataFromCallable(editText: EditText) : Callable<Unit> {
+        val callable : Callable<Unit> = Callable {
+            fun call(): Int {
+                var result = 0
+                for (x in 2..(editText.text.toString()).toInt()) {
+                    result = result + x
+                }
+                return result
+            }
+        }
+        return callable
     }
 }
